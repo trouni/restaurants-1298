3e5d625b407099834e23c34c567278d9d0970363
Trouni Tiet <trouni@gmail.com>
2023-08-16 10:41:08 +0900

    Add geocoder and mapbox

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,9 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy # restaurant.reviews
   # validates :column_name, validation
   validates :name, presence: true
+
+  # geocoded_by <name of the column where the address is stored>
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+  # after_validation(:geocode, { if: :will_save_change_to_address? })
 end
